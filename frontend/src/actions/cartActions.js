ccd5205f40a5cd8e8ed47205b17dfd5422665a2d
muhammad-asher <muhammadasher.se@gmail.com>
2023-06-09 12:42:05 +0500

    Add To Cart Functionality

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -2,21 +2,19 @@ import axios from 'axios'
 import { CART_ADD_ITEM } from '../constants/cartConstants'
 
 
-export const addtoCart = (id,qty) => async(dispatch,getState) => {
-        const {data} = await axios.get(`/api/product/${id}`)
+export const addToCart = (id, qty) => async (dispatch, getState) => {
+    const { data } = await axios.get(`/api/products/${id}`)
 
-        dispatch({
-            type:CART_ADD_ITEM,
-            paylaod:{
-                product:data._id,
-                name:data.name,
-                image: data.image,
-                price:data.price,
-                countInStock:data.countInStock,
-                qty
-            }
-        })
-
-        localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems))
-
+    dispatch({
+        type: CART_ADD_ITEM,
+        payload: {
+            product: data._id,
+            name: data.name,
+            image: data.image,
+            price: data.price,
+            countInStock: data.countInStock,
+            qty
+        }
+    })
+    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
 }
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,7 +1,34 @@
 import React from 'react'
+import { useEffect } from 'react' 
+import { useDispatch, useSelector } from 'react-redux'
+import { Link, useParams } from 'react-router-dom'
+import { Row,Col, ListGroup, Image,Form ,Button } from 'react-bootstrap'
+import { addToCart } from '../actions/cartActions'
+
 
 const CartScreen = () => {
-  return (
+
+    const {id} = useParams();
+    const searchParams = new URLSearchParams(window.location.search);
+
+    const qty  = searchParams.get('qty');
+
+    const dispatch = useDispatch()
+
+    const cart = useSelector(state => state.cart)
+
+    const {cartItems} =  cart
+
+    console.log('cartItems',cartItems)
+
+    useEffect(()=>{
+        if(id) {
+            dispatch(addToCart(id,qty))
+        }
+    },[dispatch, id, qty])
+
+
+  return ( 
     <div>Cart</div>
   )
 }
